7a32a96f06dbc700b043ebe9178448d95bb57737
David Lejeune <dl@qbmt.be>
2017-08-31 11:40:56 +0200

    posh-git

--- linuxdo-dl.sh
+++ linuxdo-dl.sh
@@ -10,6 +10,10 @@ lib/show_logo $msg
 msg='Run Installers'
 lib/show_logo $msg
 
+echo 'First things first , making sure evertyhing is up-to-date'
+sudo apt-get update
+sudo apt-get upgrade
+
 echo 'Checking which install scripts will be run ...'
 run-parts --test lib/install
 echo ''
